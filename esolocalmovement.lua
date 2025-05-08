a2d959d97f2eb84e7f840f3795b65d446930e7f0
LiKTOi1029 <testingboy5689@gmail.com>
2025-05-09 00:26:04 +0330

    esolocalmovement.lua preparing for testing

--- esolocalmovement.lua
+++ esolocalmovement.lua
@@ -1,8 +1,26 @@
 function Beginning()
-	
+	local TileSet = {{["Name"] = "Grass", ["Occupied"] = "G"},{["Name"] = "Water", ["Occupied"] = "No"}}
+	local Map = {{TileSet[1],TileSet[1],TileSet[1]},
+	{TileSet[1],TileSet[2],TileSet[1]},
+	{TileSet[1],TileSet[1],TileSet[1]},}
+	-- The map is a 3x3 area.
+	for num1 = 1, #Map, 1 do
+		for num2 = 1, #Map[num1], 1 do
+			if Map[num1][num2]["Name"] == "Grass" then io.write("G ")
+			elseif Map[num1][num2]["Name"] == "Water" then io.write("W ") end
+		end
+		io.write("\n")
+	end
 end
+Beginning()
 --[[
 	plans
+	
+	Additions after coming back and re-reading this: I would like to be able to just create some sort of code-like
+	format that can easily be created through somekind of level editor and then inputted into the game. So, like a
+	map editor of sorts. This map editor will output a file with the necessary data of how the game is supposed to
+	operate.
+	
 	I am going to first and foremost bring back the Table of Tables architecture for the map display, however with one
 	key caveat in mind that instead of binding the player position to the map directly, the map will simply be a set of
 	coordinates and the player, enemy, and other entity positions can only be placed on those listed coordinates alone.
@@ -22,4 +40,6 @@ end
 	
 	Something to think about is whether I do a YOUGOIGO turn system or a WEGO system. Is Demise of Nations' WEGO better
 	or worse? And why?
+	
+	Demise of Nations' WEGO system is best because it gives both players the ability to take damage and do damage.
 ]]
